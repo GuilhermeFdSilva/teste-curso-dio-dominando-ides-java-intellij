6ea1d9fabbd1af0b7bf85437c69f66d034f323c0
Guilherme <francaguilherme27@gmail.com>
2022-09-30 22:52:17 -0300

    Debug em InlelliJ

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,34 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int a, b;
+        System.out.print("Digite o primeiro valor: ");
+        a = ent.nextInt();
+        System.out.print("Digite o segundo valor: ");
+        b = ent.nextInt();
+        int somar = somar(a, b);
+        int subitrair = subitrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        double dividir = dividir(a, b);
+        System.out.println("soma: " + somar);
+        System.out.println("subtração: " + subitrair);
+        System.out.println("multiplicação: " + multiplicar);
+        System.out.println("divisão: " + dividir);
+    }
+    public static int somar(int a, int b){
+        return a + b;
+    }
+    public static int subitrair(int a, int b){
+        return a - b;
+    }
+    public static int multiplicar(int a, int b){
+        return a * b;
+    }
+    public static double dividir(int a, int b){
+        return (double)a / b;
+    }
+}
